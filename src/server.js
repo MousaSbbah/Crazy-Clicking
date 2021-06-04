400178d475f40052374978957554c71071d57df9
mousasbbah <mosassbbah@gmail.com>
2021-06-04 03:26:17 +0300

    Prep To apply the socket

--- src/server.js
+++ src/server.js
@@ -1,27 +1,47 @@
 'use strict';
 
 const express = require('express');
+require('dotenv').config();
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
+app.use(express.static('./public'));
+app.get('/start', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 
+io.on('connection', function (socket) {
+  console.log('A user has connected');
+  socket.on('disconnect', () => {
+    console.log('A user has disconnected.');
+  });
+  socket.on('startGame', function () {
+    io.emit('upScreen');
+  });
+  socket.on('selectShape', (data) => {
+    io.emit('start',data);
+  });
+  socket.on('catched', (data) => {
+    io.emit('hide',data);
+  });
+  socket.on('createFirstShape', (data) => {
+    io.emit('renderFirstShape',data);
+  });
+  socket.on('addTowShape', (data) => {
+    io.emit('renderNewShapes',data);
+  });
+});
 
+/////////////////////////
+function start(port) {
 
-app.use(express.static('./public'))
-
-app.get('/',(req,res)=>{
-  res.status(200).send('Server Is Working')
-})
-
-
-
-
-
-function start(port){
-  app.listen(port,()=>{
-    console.log('Server listening at PORT : '+ port)
-  })
+  server.listen(port, () => {
+    console.log(`::::: Up  on PORT ${port} :::::`);
+  });
 }
-module.exports ={
-  server: app,
-  start: start
+module.exports={
+  server,start
 }
